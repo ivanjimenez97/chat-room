274dd42fdb34853a127ec06848d45346118807c4
Ivan Jimenez <ivaneduardojimenezleon@gmail.com>
2024-06-01 14:01:58 -0600

    vite.config.js file was updated to make server proxy configurations.

--- config.cjs
+++ config.cjs
@@ -1,8 +1,7 @@
-// config.js
-
+// config.cjs
 module.exports = {
   APP_NAME: process.env.APP_NAME,
   SERVER_PORT: process.env.SERVER_PORT || 3000,
-  SERVER_DOMAIN: process.env.SERVER_DOMAIN || "http://localhost:3000",
+  SERVER_DOMAIN: process.env.SERVER_DOMAIN || "http://localhost:4000",
   DEV_URL: process.env.DEV_URL,
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 //Loading .env variables
-import config from "../config";
+import { SERVER_DOMAIN } from "../config.cjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/socket.io": {
-        target: `${config.SERVER_DOMAIN}`,
+        target: SERVER_DOMAIN,
         ws: true,
       },
     },
